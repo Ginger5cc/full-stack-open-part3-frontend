75af87f7b745e6d72e3a3b7f63f131c656238420
Ginger <gingerginger5cc@gmail.com>
2024-06-26 13:19:58 -0400

    ex2.9 - add search bar function and it work

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,35 @@ const ShowName = ({person}) => {
   )
 }
 
+const Fil = (props) => {
+  const filteredData = props.persons.filter(person => {
+    if (props.input === '') {return person}
+    else if (person.name.toLowerCase().includes(props.input))
+      {return person}
+    }
+  )
+  console.log(filteredData)
+  return (
+      filteredData.map(person => <ShowName key={person.id} person={person} />)
+  )
+  
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('new name')
   const [newNumber, setNewNumber] = useState('new number')
+  const [filter, setFilter] = useState('')
+  
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
@@ -21,6 +44,9 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+
+
+
   const addName = (event) => {
     event.preventDefault()
     if (persons.find(person => JSON.stringify(newName) === JSON.stringify(person.name)))
@@ -28,7 +54,8 @@ const App = () => {
 
     const nameObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(nameObject))
     setNewName('')
@@ -38,6 +65,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with
+          <input
+            value={filter}
+            onChange={handleFilterChange}
+          />
+        </div>
+      <h3>Add a new</h3>
       <form>
         <div>
           name: 
@@ -57,7 +92,11 @@ const App = () => {
           <button onClick={addName} type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Filtered</h3>
+      <ul>
+      <Fil input={filter} persons={persons}/>
+      </ul>
+      <h3>All Numbers</h3>
       <ul>
       {persons.map(person => <ShowName key={person.id} person={person} />)}
       </ul>
